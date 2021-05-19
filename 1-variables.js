73369f0487314b51ce6d79de4ceec1c565c8a9ea
ybekardev <y_bekar@hotmail.com>
2021-05-18 19:16:25 -0500

    initial commit

---
+++ 1-variables.js
@@ -0,0 +1,15 @@
+console.log('Hello World!');
+
+var quantity;
+quantity = 5;
+console.log(quantity);
+
+var i = 100;
+i = 10;
+console.log(i);
+
+var price = 5, quantity = 'string';
+console.log(price, quantity);
+console.log(price + 10);
+
+//variable naming rules are the same as Java language
