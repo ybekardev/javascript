95b823e33a21ce3a0c3cb57e95d2fdbb5d51f15e
ybekardev <y_bekar@hotmail.com>
2021-05-18 19:43:46 -0500

    added variables

---
+++ 2-operators.js
@@ -0,0 +1,20 @@
+//Aritmetic
+console.log(3 + 5 * 5 - 4);
+
+//Assignment
+var x = 5;
+x += 5;
+
+//Comaprison
+var one = 1;
+var two = 1;
+var three = '1';
+console.log(one == two);
+console.log(one === two);
+console.log(one === three); //checking if the value and types are same
+console.log(one == two); //checking if the value are same
+
+//Logical operators
+console.log(5 < 2);
+console.log((4 == 3) && (2 === 2));
+console.log((5 >= 6) || (4 <= 4));
---
+++ 3-controlFlow.js
@@ -0,0 +1,13 @@
+var num1  = 40;
+var num2 = 40
+
+if(num1 > num2){
+  console.log('num1 is greater');
+}else {
+  console.log('num2 is greater or eaqual');
+}
+
+//Ternary
+var age = 25;
+var allowRegister = age > 18 ? true : false
+console.log(allowRegister);
